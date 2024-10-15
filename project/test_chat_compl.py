6dd4a88c2c41446869e4af59340b356f28f5174b
Emery <emery2call@gmail.com>
2024-10-15 12:40:44 -0600

    switched model to code llama instruct

--- project/test_chat_compl.py
+++ project/test_chat_compl.py
@@ -7,8 +7,15 @@ from llama_cpp import Llama # for usage see https://llama-cpp-python.readthedocs
 
 
 llm = Llama.from_pretrained(
-	repo_id="ggml-org/Meta-Llama-3.1-8B-Instruct-Q4_0-GGUF",
-	filename="meta-llama-3.1-8b-instruct-q4_0.gguf",
+	# repo_id="ggml-org/Meta-Llama-3.1-8B-Instruct-Q4_0-GGUF",
+	# filename="meta-llama-3.1-8b-instruct-q4_0.gguf",
+
+	# repo_id="TheBloke/CodeLlama-7B-GGUF",
+    # filename="codellama-7b.Q4_K_M.gguf",
+    
+	repo_id="TheBloke/CodeLlama-7B-Instruct-GGUF",
+    filename="codellama-7b-instruct.Q4_K_M.gguf", # pick the balanced quantized
+
     verbose=True, # to view info about ai as it runs and see accel mode
     #   n_gpu_layers=1, # Uncomment to use GPU acceleration
       # seed=1337, # Uncomment to set a specific seed
@@ -17,7 +24,7 @@ llm = Llama.from_pretrained(
 
 
 message_history = [
-    {"role": "system", "content": "You are an assistant that helps answer trivia about automated vulnerability discovery techniques."},
+    {"role": "system", "content": "You are an AI that identifies vulnerabilities in code."},
 ]
 
 while True:
