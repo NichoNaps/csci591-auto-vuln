0b582f3fa4432e0267320184fb696ec068ca5591
Emery <emery2call@gmail.com>
2024-10-29 15:24:13 -0600

    renamed and moved plot into interpreter

--- homework3/graph.py
+++ homework3/graph.py
@@ -1,14 +1,7 @@
-import networkx as nx
-import matplotlib.pyplot as plt
-from test2 import Interpreter
+from interpreter import Interpreter
 from runner import parseSourceCode
 
-# my tkinter install is messed up but this seems to make it work
-import tkinter 
 
-# pip install pygraphviz networkx matplotlib 
-# apt install pyhton3-tk ??
-# apt install graphviz ???
 
 
 func_def = parseSourceCode(
@@ -23,19 +16,12 @@ int f(int x, int y) {
         y = x - y;
         x = x - y;
 
-        if (x > y) {
+        if (1 == 1) {
             int x = 5;
             return 0;
         }
     }
 
-    int f = 3333;
-    if (1 == x) {
-
-    }
-    if (1 == y) {
-
-    }
 
     return 1;
 }
@@ -43,39 +29,7 @@ int f(int x, int y) {
 
 res = Interpreter.startOnFunction(func_def)
 
-
-G = nx.DiGraph()
-
-
-# Parse over the resulting symbolic states turning them into a directed graph with networkx
-def parseRes(parent):
-
-    for child in parent.children:
-
-        print(child.id)
-        G.add_node(child.id, label=str(child))
-        G.add_edge(parent.id, child.id, label=child.edgeConstraint)
-
-        parseRes(child)
-    
-G.add_node(res.id, label=str(res))
-parseRes(res)
+res.plot()
 
 
-# pos = nx.spring_layout(G, k=0.5, seed=1)
-# pos = nx.shell_layout(G)
-pos = nx.nx_agraph.graphviz_layout(G, prog="dot") # use graphviz for its tree layout func
-
-nx.draw(G, pos, with_labels=False, node_size=5000, node_shape="s", node_color="lightblue")
-
-# Draw edge labels
-nx.draw_networkx_edge_labels(G, pos, edge_labels=nx.get_edge_attributes(G, "label"), font_color="red")
-
-# Draw node labels
-nx.draw_networkx_labels(G, pos, labels=nx.get_node_attributes(G, "label"), font_color="blue", font_size=8)
-
-plt.title("Symbolic Execution Diagram")
-plt.show()
-# plt.savefig("test.png", format="PNG")
-# plt.close()
 
--- homework3/interpreter.py
+++ homework3/interpreter.py
@@ -145,6 +145,52 @@ class Interpreter:
         return newInterp
     
 
+    def plot(self):
+        # pip install pygraphviz networkx matplotlib 
+        # apt install pyhton3-tk ??
+        # apt install graphviz ???
+        
+        import networkx as nx
+        import matplotlib.pyplot as plt
+        # my tkinter install is messed up but this seems to make it work
+        import tkinter 
+
+        G = nx.DiGraph()
+
+        # Parse over the resulting symbolic states turning them into a directed graph with networkx
+        def parseRes(parent):
+
+            for child in parent.children:
+
+                print(child.id)
+                G.add_node(child.id, label=str(child))
+                G.add_edge(parent.id, child.id, label=child.edgeConstraint)
+
+                parseRes(child)
+            
+        G.add_node(self.id, label=str(self))
+        parseRes(self)
+
+
+        # pos = nx.spring_layout(G, k=0.5, seed=1)
+        # pos = nx.shell_layout(G)
+        pos = nx.nx_agraph.graphviz_layout(G, prog="dot") # use graphviz for its tree layout func
+
+        nx.draw(G, pos, with_labels=False, node_size=5000, node_shape="s", node_color="lightblue")
+
+        # Draw edge labels
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=nx.get_edge_attributes(G, "label"), font_color="red")
+
+        # Draw node labels
+        nx.draw_networkx_labels(G, pos, labels=nx.get_node_attributes(G, "label"), font_color="blue", font_size=8)
+
+        plt.title("Symbolic Execution Diagram")
+        plt.show()
+        # plt.savefig("test.png", format="PNG")
+        # plt.close()
+
+
+
     #@TODO: we have combine arithmetic and condition expression together into the same thing. Ex: x+y > 0. Currently we make a distinction between them.
     #@TODO: this doesn't implement truthiness yet (integers == 0 are false, all other values are true)
     def parseConditionExpressionToZ3(self, exp: Node):
@@ -383,6 +429,7 @@ class Interpreter:
                 self.node = self.node.next_sibling
 
             # input('Press enter to continue...')
+
 
 
     # Create a new interpreter and run it on a function
