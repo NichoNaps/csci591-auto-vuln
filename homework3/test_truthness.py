9a3e3e0d5822b3d32c089b5c7c7020f1e9d18499
Emery <emery2call@gmail.com>
2024-10-29 15:28:57 -0600

    added test file for truthiness

--- homework3/test_truthness.py
+++ homework3/test_truthness.py
@@ -7,21 +7,10 @@ from runner import parseSourceCode
 func_def = parseSourceCode(
 """
 int f(int x, int y) {
-    int z = 5;
 
-    if (x > y) {
-        int x = x;
-
-        x = x + y;
-        y = x - y;
-        x = x - y;
-
-        if (1 == 1) {
-            int x = 5;
-            return 0;
-        }
+    if (x || y > x) {
+        return 0;
     }
-
 
     return 1;
 }
