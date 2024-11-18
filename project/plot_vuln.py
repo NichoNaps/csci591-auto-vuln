80b20d6698d185bab6bd8dc93aa00521bf8a9cff
Emery <emery2call@gmail.com>
2024-11-18 09:48:31 -0700

    added computation of stats for vuln

--- project/plot_vuln.py
+++ project/plot_vuln.py
@@ -4,6 +4,8 @@ import copy
 import numpy as np
 import tkinter # fix plt
 
+# This file computes stats and plots vulnerability detection
+
 default_frequencies = {
     'true_pos': 0,
     'true_neg': 0,
@@ -31,6 +33,25 @@ for file in resultsPath.iterdir():
             variantResults[variant][res] += 1
 
 
+
+
+for idx, (variant, freqs) in enumerate(variantResults.items()):
+
+    # https://machinelearningmastery.com/precision-recall-and-f-measure-for-imbalanced-classification/
+
+    #@TODO what do we do with invalid_response?
+
+    # higher is better for all of these
+    recall = freqs['true_pos'] / (freqs['true_pos'] + freqs['false_neg'])
+    precision = freqs['true_pos'] / (freqs['true_pos'] + freqs['false_pos'])
+    F1 = 2 * (precision * recall) / (precision + recall) # f1 represents a combination of both precision and recall
+
+    # print stats
+    print()
+    print(variant)
+    print(f"Recall: {recall}")
+    print(f"Precision: {precision}")
+    print(f"F1: {F1}")
 
 
 
